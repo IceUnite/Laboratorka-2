583753cfb064e12ae04b894b71a6d3f720fedfa2
IceUnite <48406977+IceUnite@users.noreply.github.com>
2019-03-17 22:16:34 +0300

    Add files via upload

---
+++ Лаба 2 задание 4.py
@@ -0,0 +1,11 @@
+print('Введите n(длину списка)')
+n = int(input())
+dict = []
+count = 0
+for i in range(0, n):
+    dict.append(int(input('Введите ' + str(i) + ' элемент списка: ')))
+print(dict)
+dict = [x for x in dict if x % 2 != 0]
+for i in range(2):
+    dict.append(int(input('Добавьте новый элемент: ')))
+print(dict)
